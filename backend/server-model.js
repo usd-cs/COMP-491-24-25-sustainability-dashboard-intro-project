5e3cf9b41bce9a45e9f7c14300f57d2fbb4f439b
Sebastian Fernandez <sebastianfernandez@sandiego.edu>
2024-11-13 16:53:16 -0800

    Added Cors to connect front end to backend

--- backend/server-model.js
+++ backend/server-model.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import auth_routes from "./auth/routes";
 import comments_routes from '../src/comments/routes';
 import posts_routes from '../src/posts/routes';
@@ -8,6 +9,7 @@ import posts_routes from '../src/posts/routes';
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/comments', comments_routes);
@@ -19,6 +21,6 @@ app.use('/api/users', user_routes);
 app.use('/auth', auth_routes);
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
