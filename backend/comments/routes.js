20ebf3e18e3901382c4d7e0912edbcb2155d2c51
Sebastian Fernandez <sebastianfernandez@sandiego.edu>
2024-11-19 18:39:20 -0800

    added backend route for deleting comment by comment id

--- backend/comments/routes.js
+++ backend/comments/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { add_comment, } from './controller.js'; // add remove comment when implemented
+import { add_comment, delete_comment } from './controller.js'; // Import the controller functions
 
 const router = Router();
 
 
-
 router.post("/add", add_comment);
+router.post("/del", delete_comment);
 
 export default router;
 
